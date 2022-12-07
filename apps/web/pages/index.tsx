e026c3bbc735e9fb5c47d086283b87107ab858db
Evgenii Khramkov <hey@ewgenius.me>
2022-12-07 10:20:40 +0400

    show picker when available

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function hex2rgb(hex: string) {
   const r16 = hex.slice(1, 2);
@@ -15,9 +15,19 @@ function hex2rgb(hex: string) {
 }
 
 export default function Web() {
+  const [showInteractivePicker, setShowInteractivePicker] = useState(false);
   const [showPicker, setShowPicker] = useState(true);
   const [colors, setColors] = useState(["#ffffff"]);
   const pushColor = (color: string) => setColors((c) => [color, ...c]);
+
+  useEffect(() => {
+    // @ts-ignore
+    if (typeof EyeDropper === "undefined") {
+      setShowInteractivePicker(false);
+    } else {
+      setShowInteractivePicker(true);
+    }
+  }, []);
 
   const pick = () => {
     // @ts-ignore
@@ -60,7 +70,7 @@ export default function Web() {
                 }}
               >
                 <div
-                  className="bg-white absolute w-[62px] h-[62px] border-l-[6px] border-b-[6px] border-[#eaeaea] top-0 right-0 transition-colors duration-300"
+                  className="bg-white absolute w-[62px] h-[62px] border-[#eaeaea] top-0 right-0 transition-colors duration-300"
                   style={{
                     background: colors[0],
                     borderColor: colors[0],
@@ -73,52 +83,56 @@ export default function Web() {
               </div>
             </div>
 
-            <div className="hidden relative md:flex flex-col gap-2 justify-center items-center">
-              <button
-                onClick={pick}
-                className="px-6 py-3 gap-4 text-lg bg-black font-mono flex justify-center items-center rounded-lg border border-zinc-800"
-              >
-                pick
-                <img src="/icon.svg" alt="logo" className="w-6 h-6" />
-              </button>
+            {(
+              <div className="hidden relative md:flex flex-col gap-2 justify-center items-center transition-opacity duration-300" style={{
+                opacity: showInteractivePicker ? 1 : 0
+              }}>
+                <button
+                  onClick={pick}
+                  className="px-6 py-3 gap-4 text-lg bg-black font-mono flex justify-center items-center rounded-lg border border-zinc-800"
+                >
+                  pick
+                  <img src="/icon.svg" alt="logo" className="w-6 h-6" />
+                </button>
 
-              <div className="absolute top-16 flex flex-col">
-                {colors.length > 1 &&
-                  colors.slice(0, 8).map((color, i) => (
-                    <div
-                      key={i}
-                      style={{
-                        opacity: (5 - i) / 5,
-                        transform: `scale(${(10 - i) / 10})`,
-                      }}
-                      className="relative px-2 py-1 flex justify-center items-center gap-2 font-mono text-xs rounded border border-zinc-900"
-                    >
-                      {i === 0 && (
-                        <svg
-                          className="w-3 h-3 absolute left-[-16px]"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                          />
-                        </svg>
-                      )}
-
-                      {color}
+                <div className="absolute top-16 flex flex-col">
+                  {colors.length > 1 &&
+                    colors.slice(0, 8).map((color, i) => (
                       <div
-                        className="w-3 h-3 rounded-sm shadow shadow-stone-700"
-                        style={{ background: color }}
-                      />
-                    </div>
-                  ))}
+                        key={i}
+                        style={{
+                          opacity: (5 - i) / 5,
+                          transform: `scale(${(10 - i) / 10})`,
+                        }}
+                        className="relative px-2 py-1 flex justify-center items-center gap-2 font-mono text-xs rounded border border-zinc-900"
+                      >
+                        {i === 0 && (
+                          <svg
+                            className="w-3 h-3 absolute left-[-16px]"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                            />
+                          </svg>
+                        )}
+
+                        {color}
+                        <div
+                          className="w-3 h-3 rounded-sm shadow shadow-stone-700"
+                          style={{ background: color }}
+                        />
+                      </div>
+                    ))}
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </main>
       </div>
