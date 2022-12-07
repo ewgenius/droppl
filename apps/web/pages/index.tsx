e8674170c3ae3d3e77be5a4c0719ae94a32c6612
Evgenii Khramkov <hey@ewgenius.me>
2022-12-07 23:35:49 +0400

    demo extension

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 function inverse(figure: number) {
   // inverse a RGB color
@@ -33,36 +33,32 @@ export default function Web() {
 
   const pushColor = (color: string) => {
     setColors((c) => [color, ...c]);
-    const inversed = inverseColor(color)
-    document.documentElement.style.setProperty("background-color", color)
+    const inversed = inverseColor(color);
+    document.documentElement.style.setProperty("background-color", color);
     document.documentElement.style.setProperty("--color", color);
     document.documentElement.style.setProperty("--color-rgb", hex2rgb(color));
-    document.documentElement.style.setProperty("--color-inverse-rgb", hex2rgb(inversed));
     document.documentElement.style.setProperty(
-      "--color-inverse",
-      inversed
-      );
+      "--color-inverse-rgb",
+      hex2rgb(inversed)
+    );
+    document.documentElement.style.setProperty("--color-inverse", inversed);
   };
 
   useEffect(() => {
-    let timer: NodeJS.Timer;
-
     // @ts-ignore
     if (typeof EyeDropper === "undefined") {
       setShowInteractivePicker(false);
-      // timer = setInterval(() => {
-      //   pushColor(
-      //     Math.round(Math.random() * 16777215)
-      //       .toString(16)
-      //       .padStart(6, "0")
-      //   );
-      // }, 3000);
     } else {
       setShowInteractivePicker(true);
     }
-
-    return () => clearInterval(timer);
   }, []);
+
+  const onChange = (color?: string) => {
+    setShowPicker(true);
+    if (color) {
+      pushColor(color);
+    }
+  };
 
   const pick = () => {
     // @ts-ignore
@@ -71,8 +67,7 @@ export default function Web() {
     dropper
       .open()
       .then(({ sRGBHex }: any) => {
-        setShowPicker(true);
-        pushColor(sRGBHex);
+        onChange(sRGBHex);
       })
       .catch(() => {
         setShowPicker(true);
@@ -85,12 +80,32 @@ export default function Web() {
         <title>Droppl</title>
       </Head>
 
-      <div
-        className="min-h-screen transition-colors duration-300"
-      >
-        <main className="mx-auto container px-16 md:px-16 pt-16 md:pt-32">
-          <div className="flex flex-col gap-16 items-start md:flex-row md:items-center justify-center">
+      <div className="min-h-screen transition-colors duration-300">
+        <main className="mx-auto container px-16 md:px-16 flex flex-col gap-4 justify-center items-center min-h-screen">
+          <div
+            className="absolute top-4 right-4 rounded-xl overflow-hidden shadow-2xl"
+            style={{
+              transform: "translateY(-150%)",
+              animationName: "slideIn",
+              animationDelay: "500ms",
+              animationFillMode: "forwards",
+              animationDuration: "500ms",
+              animationTimingFunction: "ease-out",
+            }}
+          >
+            <App onChange={onChange} onPick={() => setShowPicker(false)} />
+          </div>
+
+          <header className="flex flex-col gap-8 md:flex-row md:items-center justify-center">
+            {/* h1 */}
             <div className="relative">
+              <p
+                style={{ color: "var(--color)" }}
+                className="opacity-75 absolute -top-8 left-3 text-xl"
+              >
+                simple color picker
+              </p>
+
               <h1
                 className="text-6xl antialiased md:text-9xl font-bold relative transition-colors duration-300"
                 style={{
@@ -100,8 +115,9 @@ export default function Web() {
                 Droppl
               </h1>
 
+              {/* pseudo picker */}
               <div
-                className="absolute scale-[0.40] md:transform-none top-[-10px] left-[-55px] md:top-[45px] md:left-[-49px] w-[120px] h-[120px] flex flex-col justify-center items-center rounded-[50%] border border-[#595a59] overflow-hidden transition-all duration-300"
+                className="absolute scale-[0.40] md:transform-none top-[-10px] left-[-55px] md:top-[45px] md:left-[-49px] w-[120px] h-[120px] flex flex-col justify-center items-center rounded-[50%] border border-[#595a59] overflow-hidden"
                 style={{
                   opacity: showPicker ? 1 : 0,
                   backgroundColor: "var(--color-inverse)",
@@ -121,73 +137,137 @@ export default function Web() {
               </div>
             </div>
 
-            {
-              <div
-                className="hidden relative md:flex flex-col gap-2 justify-center items-center transition-opacity duration-300"
-                style={{
-                  opacity: showInteractivePicker ? 1 : 0,
-                }}
-              >
-                <button
-                  onClick={pick}
-                  className="text-white px-5 py-3 gap-4 text-lg bg-black font-mono flex justify-center items-center rounded-lg border border-zinc-800"
-                >
-                  pick
-                  <img src="/icon.svg" alt="logo" className="w-6 h-6" />
-                </button>
+            {/* logo */}
+            <div className="relative flex flex-col gap-2 justify-center items-center transition-opacity duration-300">
+              <img
+                src="/icon.svg"
+                alt="logo"
+                className="w-20 md:w-32 h-20 md:h-32"
+              />
+              <div className="absolute top-24 md:top-36 flex flex-col">
+                {colors.length > 1 &&
+                  colors.slice(0, 5).map((color, i) => {
+                    const inverted = inverseColor(color);
+                    return (
+                      <div
+                        key={i}
+                        className="relative px-2 py-1 flex justify-center items-center gap-2 font-mono text-xs rounded border border-zinc-900 transition-colors duration-300"
+                        style={{
+                          opacity: (8 - i) / 5,
+                          transform: `translateY(${-i * 16}px) scale(${
+                            (10 - i) / 10
+                          })`,
+                          zIndex: 8 - i,
+                          color: inverted,
+                          backgroundColor: color,
+                        }}
+                      >
+                        {i === 0 && (
+                          <svg
+                            className="w-3 h-3 absolute left-[-16px]"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke={color}
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                            />
+                          </svg>
+                        )}
 
-                <div className="absolute top-16 flex flex-col">
-                  {colors.length > 1 &&
-                    colors.slice(0, 5).map((color, i) => {
-                      const inverted = inverseColor(color);
-                      return (
+                        {color}
                         <div
-                          key={i}
-                          className="relative px-2 py-1 flex justify-center items-center gap-2 font-mono text-xs rounded border border-zinc-900 transition-colors duration-300"
+                          className="w-3 h-3 rounded-sm border"
                           style={{
-                            opacity: (8 - i) / 5,
-                            transform: `translateY(${-i * 16}px) scale(${
-                              (10 - i) / 10
-                            })`,
-                            zIndex: 8 - i,
-                            color: inverted,
-                            backgroundColor: color,
+                            background: color,
+                            borderColor: inverted,
                           }}
-                        >
-                          {i === 0 && (
-                            <svg
-                              className="w-3 h-3 absolute left-[-16px]"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={1.5}
-                              stroke={color}
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                              />
-                            </svg>
-                          )}
-
-                          {color}
-                          <div
-                            className="w-3 h-3 rounded-sm border"
-                            style={{
-                              background: color,
-                              borderColor: inverted,
-                            }}
-                          />
-                        </div>
-                      );
-                    })}
-                </div>
+                        />
+                      </div>
+                    );
+                  })}
               </div>
-            }
-          </div>
+            </div>
+          </header>
         </main>
       </div>
     </>
   );
 }
+
+export interface AppProps {
+  onPick: () => void;
+  onChange: (color?: string) => void;
+}
+
+const App: FC<AppProps> = ({ onPick, onChange }) => {
+  const [colors, setColors] = useState<Array<string>>([]);
+
+  const pick = () => {
+    // @ts-ignore
+    const dropper = new EyeDropper();
+
+    onPick();
+    dropper
+      .open()
+      .then(({ sRGBHex }: any) => {
+        setColors((c) => [sRGBHex, ...c]);
+        onChange(sRGBHex);
+      })
+      .catch(() => {
+        onChange();
+      });
+  };
+
+  return (
+    <div className="bg-white shadow-white w-[168px] flex flex-col text-sm font-mono">
+      <div className="flex flex-col p-2 gap-2">
+        <div className="flex gap-2">
+          <button
+            onClick={pick}
+            className="flex justify-center ring-2 ring-amber-400 items-center border border-amber-600 text-amber-600 rounded-md p-2 text-sm"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="w-4 h-4"
+            >
+              <path d="M12 2.69l5.66 5.66a8 8 0 1 1-11.31 0z"></path>
+            </svg>
+          </button>
+          <span className="flex justify-center items-center bg-gray-200 text-gray-500 flex-grow border border-gray-300 rounded-md px-2 py-1 text-sm">
+            {colors[0]}
+          </span>
+        </div>
+        {colors.length > 0 && (
+          <div className="flex gap-2 flex-wrap">
+            {colors.map((color, i) => (
+              <div
+                key={i}
+                className="w-6 h-6 rounded-md border border-gray-200 shadow"
+                style={{
+                  backgroundColor: color,
+                }}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="p-2 border-t border-amber-300 bg-amber-200 text-gray-400 shadow-inner text-xs">
+        droppl, 0.0.1
+      </div>
+    </div>
+  );
+};
